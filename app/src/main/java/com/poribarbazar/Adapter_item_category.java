9fe968fde32d04314a9c87a82ef0763f8e800091
tanveerhasan <tanveer.cse01@gmail.com>
2021-02-21 17:03:26 +0600

    done flash_sell_adapter Class

---
+++ app/src/main/java/com/poribarbazar/Adapter_item_category.java
@@ -0,0 +1,77 @@
+package com.poribarbazar;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.poribarbazar.model.ModelProducts;
+
+import java.util.ArrayList;
+
+
+public class Adapter_item_category extends RecyclerView.Adapter<Adapter_item_category.Holder> {
+
+    ArrayList<ModelProducts> products;
+    Context context;
+
+    public Adapter_item_category(ArrayList<ModelProducts> products, Context context) {
+        this.products = products;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Adapter_item_category.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_category_products,parent,false);
+
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter_item_category.Holder holder, int position) {
+
+
+
+        holder.p_name.setText(products.get(position).getName());
+      /*  holder.p_normal_price.setText(products.get(position).getSellPrice());
+        holder.p_discount.setText(products.get(position).getd);*/
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+
+        TextView p_name,p_normal_price,p_final_price,p_quantity,p_discount;
+        ImageView p_image,p_plus,p_minus,price_cross;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+
+            p_name=itemView.findViewById(R.id.item_category_name);
+            p_normal_price=itemView.findViewById(R.id.item_category_normal_price);
+            p_final_price=itemView.findViewById(R.id.item_category_final_price);
+            p_quantity=itemView.findViewById(R.id.item_category_quantity);
+            p_plus=itemView.findViewById(R.id.item_category_plus);
+            p_minus=itemView.findViewById(R.id.item_category_minus);
+            p_discount=itemView.findViewById(R.id.item_category_discount);
+            p_image=itemView.findViewById(R.id.item_category_image);
+            price_cross=itemView.findViewById(R.id.item_category_cross);
+
+
+
+        }
+    }
+}
--- app/src/main/java/com/poribarbazar/Fragment/HomeFragment.java
+++ app/src/main/java/com/poribarbazar/Fragment/HomeFragment.java
@@ -11,17 +11,25 @@ import android.widget.EditText;
 
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 
+import com.poribarbazar.Adapter_item_category;
 import com.poribarbazar.R;
 import com.poribarbazar.databinding.FragmentHomeBinding;
+import com.poribarbazar.model.ModelProducts;
+import com.poribarbazar.network.ApiClient;
+import com.poribarbazar.network.ApiInterface;
+
+import java.util.ArrayList;
 
 
 public class HomeFragment extends Fragment {
 
 private FragmentHomeBinding binding;
-
-EditText editTextsearch;
+Adapter_item_category adapter_item_category;
+ApiInterface apiInterface;
+ArrayList<ModelProducts>products;
 
     public HomeFragment() {
 
@@ -46,6 +54,15 @@ EditText editTextsearch;
         }*/
 
 
+        products=new ArrayList<>();
+
+        adapter_item_category = new Adapter_item_category(products,getContext());
+        binding.recylerFlashSell.setLayoutManager(new GridLayoutManager(getContext(),1));
+
+        apiInterface = ApiClient.getApiInterface();
+
+
+        get_flash_sell();
 
 
 
@@ -53,6 +70,11 @@ EditText editTextsearch;
     }
 
 
+    public void get_flash_sell(){
+
+
+
+    }
 
 
 }
