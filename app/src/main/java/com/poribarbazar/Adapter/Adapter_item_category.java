a3342e8e4623986b0893283d377b52782ca81e5d
Shihab <mahbubul15-8821@diu.edu.bd>
2021-03-14 15:52:08 +0600

    Adapter_item_category add ViewBinding

--- app/src/main/java/com/poribarbazar/Adapter/Adapter_item_category.java
+++ app/src/main/java/com/poribarbazar/Adapter/Adapter_item_category.java
@@ -18,6 +18,8 @@ import com.poribarbazar.Fragment.ProductFragment;
 import com.poribarbazar.MainActivity;
 import com.poribarbazar.R;
 import com.poribarbazar.UI.ProductsActivity;
+import com.poribarbazar.databinding.ItemCartBinding;
+import com.poribarbazar.databinding.ItemCategoryBinding;
 import com.poribarbazar.model.ModelCategory;
 import com.poribarbazar.model.ModelProducts;
 
@@ -39,30 +41,35 @@ public class Adapter_item_category extends RecyclerView.Adapter<Adapter_item_cat
     @NonNull
     @Override
     public Adapter_item_category.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_category,parent,false);
+       // View view = LayoutInflater.from(context).inflate(R.layout.item_category,parent,false);
+      //return new Holder(ItemCartBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false));
+        ItemCategoryBinding binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
 
-        return new Holder(view);
+      return new Holder(binding);
+       // return new Holder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Adapter_item_category.Holder holder, int position) {
 
 
-        holder.category_name.setText(categories.get(position).getCategory());
+       // holder.category_name.setText(categories.get(position).getCategory());
+
+        holder.binding.categoryName.setText(categories.get(position).getCategory());
 
         String getcat = categories.get(position).getCategory();
 
         Glide.with(context)
                 .load(categories.get(position).getImage_url())
                 .override(300, 200)
-                .into(holder.category_image);
+                .into(holder.binding.categoryImage);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent intent=new Intent(context, ProductsActivity.class);
-                intent.putExtra("category",holder.category_name.getText().toString());
+                intent.putExtra("category",holder.binding.categoryName.getText().toString());
                 context.startActivity(intent);
 
 
@@ -80,10 +87,15 @@ public class Adapter_item_category extends RecyclerView.Adapter<Adapter_item_cat
 
     public class Holder extends RecyclerView.ViewHolder {
 
+        ItemCategoryBinding binding;
         TextView category_name;
         ImageView category_image;
 
-        public Holder(@NonNull View itemView) {
+        public Holder(ItemCategoryBinding binding){
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+        /*public Holder(@NonNull @NonNull ItemCartBinding itemView) {
             super(itemView);
 
             category_name=itemView.findViewById(R.id.category_name);
@@ -92,6 +104,7 @@ public class Adapter_item_category extends RecyclerView.Adapter<Adapter_item_cat
 
 
 
-        }
+        }*/
+
     }
 }
